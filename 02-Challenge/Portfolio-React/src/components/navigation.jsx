3ab8a7abaa24bee5c08054fa3ebbc04bef9a52b8
Santy520 <sspalacio20@gmail.com>
2024-06-02 23:47:40 -0400

    made finishing touches to the project

--- 02-Challenge/Portfolio-React/src/components/navigation.jsx
+++ 02-Challenge/Portfolio-React/src/components/navigation.jsx
@@ -1,21 +1,3 @@
-// import { Link } from 'react-router-dom';
-// function Navigation() {
-
-
-//     return (
-//         <navigation>
-//            <div className="img">
-//  <Link className='img'img src="../assets/aXqYR1.png" alt="Banner-photo"></Link> 
-//   <div className="text">
-//     <h3>Eighty percent of success is showing up!</h3>
-//   </div>
-// </div>
-//         </navigation>
-//     )
-// }
-
-// export default Navigation
-
 import image from "../assets/aXqYR1.png"
 
 function Navigation() {
